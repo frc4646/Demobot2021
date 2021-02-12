0e556bd8af3409ef035682803f3bdd5a718ca89b
Roba Abbajabal <32786155+abbajabalroba@users.noreply.github.com>
2021-02-11 20:28:33 -0600

    Initial commit.\n Working on creating Demobot in new command base.

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+    //Examples: "speeds, unit conversion factors, PID gains, and sensor/motor ports"
+    //- WPILib https://docs.wpilib.org/en/stable/docs/software/commandbased/structuring-command-based-project.html#constants
+
+    public static final int frontLeftDrivePort = 3;
+    public static final int frontRightDrivePort = 2;
+    public static final int backLeftDrivePort = 1;
+    public static final int backRightDrivePort = 0;
+    public static final int flagMotorPort = 4;
+
+    public static final int gamepadPort = 0;
+    public static final int analogGyroPort = 0;
+
+    public static final int pixyPanPort = 7;
+    public static final int pixyTiltPort = 8;  
+
+    public static final int[] leftEncoderValues = {2, 3};
+    public static final int[] rightEncoderValues = {0, 1};
+}
